81d5bd11ed743d5372c75c021c4a1227b9bc5442
Ramil <rdgalimov@yandex.ru>
2022-11-28 00:59:23 +0300

    Домашнее задание. Циклы 2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,27 @@ public class Main {
         System.out.println("Hello world!");
 
         //Задача 1
-        System.out.println("Домашнее задание 3.Задание 1");
-        int input = 29000;
+        System.out.println("Домашнее задание 1.Задание 1");
+        int input = 15000;
         int total = 0;
-        for (int i = 0; i < 12; i++){
+        int i = 0;
+        while (total < 2459000) {
+            i++;
             total = total + input;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
+        System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         //Задача 2
-        System.out.println("Домашнее задание 3.Задание 2");
-        int input1 = 29000;
-        int total1 = 0;
-        for (int i = 0; i <= 12; i++){
-            total1 = total1 + total1/100;
-            total1 = total1 + input1;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
+        System.out.println("Домашнее задание 1.Задание 2");
+        int a = 0;
+        while (a < 10) {
+        a++;
+        System.out.print(" " + a);
         }
+        System.out.println("");
+        int b = 10;
+        for (; b > 0; b--) {
+        System.out.print(" " + b);
+        }
+
     }
 }
