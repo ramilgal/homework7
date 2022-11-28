d4c753005babc7f70313827837dfb1f8afdeb842
Ramil <rdgalimov@yandex.ru>
2022-11-28 09:56:47 +0300

    Домашнее задание. Циклы 2

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,16 @@ public class Main {
         for (; b > 0; b--) {
         System.out.print(" " + b);
         }
-
+        System.out.println(" ");
+        //Задача 3
+        System.out.println("Домашнее задание 1.Задание 3");
+        int totalPopulation = 12000000;
+        int birth = (totalPopulation / 1000) * 17;
+        int death = (totalPopulation / 1000) * 8;
+        int increase = birth - death;
+        for (int period = 1; period <= 10; period++) {
+            totalPopulation = totalPopulation + increase;
+            System.out.println("Год " + period + " численность населения составляет " + totalPopulation + " человек");
+        }
     }
 }
